f8132b66ee6d4719ee065cb0e69fc71a5498284e
mihai.moiseanu <paraplegie1>
2020-09-11 14:16:19 +0300

    move some channels to flows

--- app/src/main/java/com/coders/chatapplication/presentation/ui/searchfriends/SearchFriendsViewModel.kt
+++ app/src/main/java/com/coders/chatapplication/presentation/ui/searchfriends/SearchFriendsViewModel.kt
@@ -7,45 +7,43 @@ import com.coders.chatapplication.domain.model.UserModel
 import com.coders.chatapplication.domain.usecase.users.SearchUsersUseCase
 import com.coders.chatapplication.presentation.commons.BaseViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
-import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class SearchFriendsViewModel @ViewModelInject constructor(
-	private val searchUsersUseCase: SearchUsersUseCase
+    private val searchUsersUseCase: SearchUsersUseCase
 ) : BaseViewModel() {
 
-	val searchedUsers = MutableLiveData<List<UserModel>>()
+    val searchedUsers = MutableLiveData<List<UserModel>>()
 
-	private val searchChannelFlow = Channel<String>(CONFLATED)
+    private val searchChannelFlow = MutableStateFlow("")
 
-	init {
-		searchChannelFlow.consumeAsFlow()
-			.debounce(500)
-			.onEach {
-				searchUsers(it)
-			}.launchIn(viewModelScope)
-	}
+    init {
+        searchChannelFlow
+            .debounce(500)
+            .onEach {
+                searchUsers(it)
+            }.launchIn(viewModelScope)
+    }
 
-	fun search(queryString: String) {
-		viewModelScope.launch(Dispatchers.IO) {
-			searchChannelFlow.send(queryString)
-		}
-	}
+    fun search(queryString: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            searchChannelFlow.value = queryString
+        }
+    }
 
-	private fun searchUsers(queryString: String) {
-		viewModelScope.launch(Dispatchers.IO) {
-			searchUsersUseCase(this, queryString) {
-				it.either(::handleFailure, ::handleSuccess)
-			}
-		}
-	}
+    private fun searchUsers(queryString: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            searchUsersUseCase(this, queryString) {
+                it.either(::handleFailure, ::handleSuccess)
+            }
+        }
+    }
 
-	private fun handleSuccess(users: List<UserModel>) {
-		searchedUsers.postValue(users)
-	}
+    private fun handleSuccess(users: List<UserModel>) {
+        searchedUsers.postValue(users)
+    }
 }
